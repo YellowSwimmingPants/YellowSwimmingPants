8f41469d5e3862444d9281fd491751ffd30208f8
fred22211269 <fred22211269@gamil.com>
2018-12-18 18:12:13 +0800

    change

--- app/src/main/java/com/ehappy/yellowswimmingpants/MainActivity.java
+++ app/src/main/java/com/ehappy/yellowswimmingpants/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // change 02
+        // change 03
     }
 }
