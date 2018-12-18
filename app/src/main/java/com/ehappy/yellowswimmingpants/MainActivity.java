c4b914b0e9f6004851443d2f033978928f154017
Bogy chen <xpopen4000@hotmail.com.tw>
2018-12-18 18:27:19 +0800

    123

--- app/src/main/java/com/ehappy/yellowswimmingpants/MainActivity.java
+++ app/src/main/java/com/ehappy/yellowswimmingpants/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // change 03
-    }
+        //bogychen   
+        }
 }
